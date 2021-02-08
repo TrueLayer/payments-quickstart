d2dc084d08b84fd3a192895eba35c59dc00518a9
tl-thomas-pearson <thomas.pearson@truelayer.com>
2021-02-08 12:20:34 +0000

    [PUX-68] Add payment-response mapping.

--- src/models/payment-response.ts
+++ src/models/payment-response.ts
@@ -1,5 +1,73 @@
-export interface PaymentResponse {
-    // TODO
+/* eslint-disable camelcase */
+
+interface SortCode {
+    type: string,
+    sort_code: string,
+    account_number: string,
 }
 
-export default PaymentResponse;
+interface Iban {
+    type: string,
+    iban: string
+}
+
+interface Bban {
+    type: string,
+    bban: string
+}
+
+interface Nrb {
+    type: string,
+    nrb: string
+}
+
+type AccountDetails = SortCode | Iban | Bban | Nrb;
+
+interface Participant {
+    name: string,
+    account: AccountDetails
+}
+
+interface SeparateReference {
+    type: string,
+    beneficiary: string,
+    remitter: string
+}
+
+interface SingleReference {
+    type: string,
+    reference: string
+}
+
+type References = SingleReference | SeparateReference;
+
+type PaymentStatus = (
+    'initiated' |
+    'cancelled' |
+    'authorisation_failed' |
+    'executing' |
+    'rejected' |
+    'executed' |
+    'expired'
+);
+
+type SupportedCurrency = 'GBP' | 'EUR';
+
+export interface PaymentResponse {
+    single_immediate_payment_id: string,
+    status: PaymentStatus,
+    initiated_at: Date,
+    amount_in_minor: number,
+    currency: SupportedCurrency,
+    provider_id: string,
+    scheme_id: string,
+    fee_option_id: string,
+    beneficiary: Participant,
+    remitter: Participant,
+    references: References
+}
+export interface PaymentResponseResult {
+    result: PaymentResponse
+}
+
+export default PaymentResponseResult;
