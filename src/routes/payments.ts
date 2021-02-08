9533e061b7c084f4e9890a8cdfd9818fede451b7
tl-thomas-pearson <thomas.pearson@truelayer.com>
2021-02-08 14:17:47 +0000

    [PUX-67] Destructure controller methods

--- src/routes/payments.ts
+++ src/routes/payments.ts
@@ -2,9 +2,12 @@ import { Router } from 'express';
 import PaymentsController from 'controllers/payments';
 
 const router = Router();
-const controller = new PaymentsController();
+const { 
+    createPayment, 
+    getPayment,
+} = new PaymentsController();
 
-router.post('/payment', controller.createPayment);
-router.get('/payment/:id', controller.getPayment);
+router.post('/payment', createPayment);
+router.get('/payment/:id', getPayment);
 
 export default router;
