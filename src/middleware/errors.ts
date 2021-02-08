26302720cc924f33e56ace6561df5e09c8d30b75
tl-thomas-pearson <thomas.pearson@truelayer.com>
2021-02-08 19:04:16 +0000

    Fix custom error handler.

--- src/middleware/errors.ts
+++ src/middleware/errors.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 export class HttpException extends Error {
     public status: number;
@@ -11,7 +11,9 @@ export class HttpException extends Error {
     }
 }
 
-export default function error (error: HttpException, _: Request, res: Response) {
-  res.status(error.status);
+// Express requires all parameters in-order to recognize the function as a error middleware type.
+// With out all 4 params express will treat as a standard middleware.
+export default function error (error: HttpException, _req: Request, res: Response, _next: NextFunction) {
+  res.status(error.status).json();
   res.json({ error: error.message }).send();
 }
