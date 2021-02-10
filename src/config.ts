2d304c337675b51dd62bfbbbace7709cdda373d7
tl-thomas-pearson <thomas.pearson@truelayer.com>
2021-02-10 11:25:01 +0000

    [PUX-74] Add beneficary account details.

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
 export default {
+  SORT_CODE: process.env.SORT_CODE || 123456,
+  ACCOUNT_NUMBER: process.env.ACCOUNT_NUMBER || 12345678,
+  WEB_HOOK_URI: process.env.WEB_HOOK_URI || '',
   CLIENT_ID: process.env.CLIENT_ID || '',
   CLIENT_SECRET: process.env.CLIENT_SECRET || '',
   PORT: process.env.PORT || 3000
