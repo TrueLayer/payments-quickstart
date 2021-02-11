dfa418ca1596d9712b2c1158fb1f75632519f4ca
tl-thomas-pearson <thomas.pearson@truelayer.com>
2021-02-11 20:49:35 +0000

    format jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,8 @@
 module.exports = {
-  roots: [
-    '<rootDir>/src'
-  ],
-  testMatch: [
-    '**/?(*.)+(spec|test).+(ts|tsx|js)'
-  ],
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest'
   },
-  moduleDirectories: [
-    'node_modules', 'src'
-  ]
+  moduleDirectories: ['node_modules', 'src']
 };
