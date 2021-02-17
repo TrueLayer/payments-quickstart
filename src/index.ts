2c55ed7d87a2214399e4525c0b3d8292b41ec752
tl-sebastien-kovacs <sebastien@truelayer.com>
2021-02-17 11:24:39 +0000

    Linting

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,8 @@ const clientSecret = config.CLIENT_SECRET;
 const redirectUri = config.REDIRECT_URI;
 
 if (!clientId || !clientSecret || !redirectUri) {
-    console.error("'CLIENT_ID', 'CLIENT_SECRET' and 'REDIRECT_URI' environment variables are required to run the server")
-    exit()
+  console.error("'CLIENT_ID', 'CLIENT_SECRET' and 'REDIRECT_URI' environment variables are required to run the server");
+  exit();
 }
-
 
 app.listen(config.PORT, () => console.info(`⚡[server]: listening on port ${config.PORT}...`));
